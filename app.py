192c4b114f5f5693b3e69fe4a91fcb4f81b3ae29
Mark Gu <67448433+markgu713@users.noreply.github.com>
2021-01-30 00:46:52 -0500

    update scripts

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import MinMaxScaler
 import os
 import pandas as pd
 import numpy as np
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 # load model
-model = load_model('model/model.h5')
+model = load_model('model/deep_learning_model.h5')
 # summarize model.
 model.summary()
 
@@ -25,13 +26,13 @@ model.summary()
 #         }
 # df = pd.DataFrame (data, columns = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol'])
 
-from sklearn.preprocessing import MinMaxScaler
 # X_scaler = MinMaxScaler().fit(df)
 # test_scaled = X_scaler.transform(df)
 # encoded_predictions = model.predict_classes(test_scaled[:2])
 # print(f"Predicted classes: {encoded_predictions}")
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -40,7 +41,7 @@ def index():
         fixed_acidity = request.form['fixed_acidity']
         volatile_acidity = request.form['volatile_acidity']
         citric_acid = request.form['citric_acid']
-        residual_sugar =request.form['residual_sugar']
+        residual_sugar = request.form['residual_sugar']
         chlorides = request.form['chlorides']
         free_sulfur_dioxide = request.form['free_sulfur_dioxide']
         total_sulfur_dioxide = request.form['total_sulfur_dioxide']
@@ -48,7 +49,8 @@ def index():
         pH = request.form['pH']
         sulphates = request.form['sulphates']
         alcohol = request.form['alcohol']
-        
+        winetype = request.form['winetype']
+
         # data = {'fixed acidity':  [fixed_acidity],
         #     'volatile acidity': [volatile_acidity],
         #     'citric acid': [citric_acid],
@@ -59,33 +61,37 @@ def index():
         #     'density': [density],
         #     'pH': [pH],
         #     'sulphates': [sulphates],
-        #     'alcohol': [alcohol]
+        #     'alcohol': [alcohol],
+        #     'winetype': [winetype]
         # }
 
         data = {'fixed acidity':  [5.8, fixed_acidity],
-            'volatile acidity': [0.300, volatile_acidity],
-            'citric acid': [0.33, citric_acid],
-            'residual sugar': [3.5, residual_sugar],
-            'chlorides': [0.033, chlorides],
-            'free sulfur dioxide': [25.0, free_sulfur_dioxide],
-            'total sulfur dioxide': [116.0, total_sulfur_dioxide],
-            'density': [0.99057, density],
-            'pH': [3.2, pH],
-            'sulphates': [0.44, sulphates],
-            'alcohol': [11.7, alcohol]
-        }
+                'volatile acidity': [0.300, volatile_acidity],
+                'citric acid': [0.33, citric_acid],
+                'residual sugar': [3.5, residual_sugar],
+                'chlorides': [0.033, chlorides],
+                'free sulfur dioxide': [25.0, free_sulfur_dioxide],
+                'total sulfur dioxide': [116.0, total_sulfur_dioxide],
+                'density': [0.99057, density],
+                'pH': [3.2, pH],
+                'sulphates': [0.44, sulphates],
+                'alcohol': [11.7, alcohol],
+                'winetype': [0, winetype]
+                }
 
-        df = pd.DataFrame (data, columns = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol'])
+        df = pd.DataFrame(data, columns=['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                                         'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'winetype'])
         X_scaler = MinMaxScaler().fit(df)
         test_scaled = X_scaler.transform(df)
         encoded_predictions = model.predict_classes(test_scaled[:2])
         if encoded_predictions[1] == 0:
             final_prediction = "Bad Quality!"
-        else: 
+        else:
             final_prediction = "Good Quality!"
-        return render_template('index.html', prediction = final_prediction, fixed_acidity = fixed_acidity)
+        return render_template('index.html', prediction=final_prediction, fixed_acidity=fixed_acidity, volatile_acidity=volatile_acidity, citric_acid=citric_acid, residual_sugar=residual_sugar, chlorides=chlorides, free_sulfur_dioxide=free_sulfur_dioxide, total_sulfur_dioxide=total_sulfur_dioxide, density=density, pH=pH, sulphates=sulphates, alcohol=alcohol, winetype=winetype)
     else:
         return render_template('index.html')
+
 
 if __name__ == "__main__":
     # app.debug = True
